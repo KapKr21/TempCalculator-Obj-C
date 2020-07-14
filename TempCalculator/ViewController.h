23969aff73e3bdc5bd5726a33948d965201ae062
KapKr21 <khatri.kapil2011@yahoo.com>
2020-07-14 17:11:45 +0530

    Initial Commit

--- TempCalculator/ViewController.h
+++ TempCalculator/ViewController.h
@@ -9,6 +9,16 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *enterLabel;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *outputLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *controlSeg;
+
+- (IBAction)segCalculator:(id)sender;
+- (IBAction)calculate:(id)sender;
+
+
 
 
 @end
--- TempCalculator/ViewController.m
+++ TempCalculator/ViewController.m
@@ -20,4 +20,120 @@
 }
 
 
+
+- (IBAction)segCalculator:(id)sender {
+    
+    if(self.controlSeg.selectedSegmentIndex == 0)
+    {
+        self.enterLabel.text = @"Enter Fahrenheit";
+        self.textField.text = @"";
+        self.outputLabel.text = @"0 Celsius";
+        self.imageView.image = [UIImage imageNamed:@"Temp1.png"];
+    }
+    else
+    {
+        self.enterLabel.text = @"Enter Celcius";
+        self.textField.text = @"";
+        self.outputLabel.text = @"0 Fahrenheit";
+        self.imageView.image = [UIImage imageNamed:@"Temp1.png"];
+        
+    }
+}
+
+- (IBAction)calculate:(id)sender {
+    
+    if(self.controlSeg.selectedSegmentIndex == 0)
+    {
+        double fharenheit = [self.textField.text doubleValue];
+        double celsius = (fharenheit-32)*5/9;
+
+        self.outputLabel.text = [NSString stringWithFormat:@"%2.2lf Celcius",celsius];
+        
+        if(celsius > 120)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp9.png"];
+        }
+        else if(celsius >100)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp8.png"];
+        }
+        else if(celsius >80)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp7.png"];
+        }
+        else if(celsius >60)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp6.png"];
+        }
+        else if(celsius >40)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp5.png"];
+        }
+        else if(celsius >20)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp4.png"];
+        }
+        else if(celsius >0)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp3.png"];
+        }
+        else if(celsius > -20)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp2.png"];
+        }
+        else if(celsius < -20)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp1.png"];
+        }
+        
+    }
+    else
+    {
+        double celcius = [self.textField.text doubleValue];
+        double fharenheit = (celcius*9/5)+32;
+        
+        self.outputLabel.text = [NSString stringWithFormat:@"%2.2lf Fharenheit",fharenheit];
+        
+        if(fharenheit > 120)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp9.png"];
+        }
+        else if(fharenheit >100)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp8.png"];
+        }
+        else if(fharenheit >80)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp7.png"];
+        }
+        else if(fharenheit >60)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp6.png"];
+        }
+        else if(fharenheit >40)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp5.png"];
+        }
+        else if(fharenheit >20)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp4.png"];
+        }
+        else if(fharenheit >0)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp3.png"];
+        }
+        else if(fharenheit > -20)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp2.png"];
+        }
+        else if(fharenheit < -20)
+        {
+            self.imageView.image = [UIImage imageNamed:@"Temp1.png"];
+        }
+        
+        
+        
+        
+    }
+}
 @end
